28bd0695788b16294b8656c4f7a44ccaf67146f9
Vincent John Cagay <vincent.cagay@virtusize.com>
2020-06-02 15:05:55 +0900

    Implement @virtusize/storybook-app-vue-next based on v4.3.19

--- app/vue/src/server/framework-preset-vue.ts
+++ app/vue/src/server/framework-preset-vue.ts
@@ -1,24 +1,16 @@
-import VueLoaderPlugin from 'vue-loader/lib/plugin';
 import { Configuration } from 'webpack';
 
 export function webpack(config: Configuration) {
   return {
     ...config,
-    plugins: [...config.plugins, new VueLoaderPlugin()],
+    plugins: [...config.plugins],
     module: {
       ...config.module,
-      rules: [
-        ...config.module.rules,
-        {
-          test: /\.vue$/,
-          loader: require.resolve('vue-loader'),
-          options: {},
-        },
-      ],
+      rules: [...config.module.rules],
     },
     resolve: {
       ...config.resolve,
-      extensions: [...config.resolve.extensions, '.vue'],
+      extensions: [...config.resolve.extensions],
       alias: {
         ...config.resolve.alias,
         vue$: require.resolve('vue/dist/vue.esm.js'),
@@ -30,6 +22,6 @@ export function webpack(config: Configuration) {
 export function babelDefault(config: any) {
   return {
     ...config,
-    presets: [...config.presets, require.resolve('babel-preset-vue')],
+    presets: [...config.presets],
   };
 }
